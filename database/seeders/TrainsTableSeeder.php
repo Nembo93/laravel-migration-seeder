b266a7fee73d02b279bba7260f4863688beb53c5
nembo93 <francesco.c193@libero.it>
2023-01-18 15:00:39 +0100

    Aggiunge fakers

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -14,8 +14,21 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i <= 7; $i++ ){
+            $new_train = new Train();
+            $new_train->company = $faker->randomElement(['Trenitalia, Italo,']);
+            $new_train->start_station = $faker->city();
+            $new_train->destination = $faker->city();
+            $new_train->trip_date = $faker->date('Y_m_d');
+            $new_train->leave_hour = $faker->time('H_i');
+            $new_train->arrival_hour = $faker->time('H_i');
+            $new_train->train_code = $faker->numerify();
+            $new_train->wagon_number = $faker->numberBetween(8, 12);
+            $new_train->in_time = $faker->numberBetween(0, 1);
+            $new_train->aborted = $faker->numberBetween(0, 1);
+            $new_train->save();
+        }
     }
 }
